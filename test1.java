ef6adc2a0dc4edddf881fc9d2bc7352b638cf932
duongcongke <103102773+duongcongke@users.noreply.github.com>
2022-04-25 10:56:06 +0700

    Create test1.java

---
+++ test1.java
@@ -0,0 +1,12 @@
+public class test1 {
+}
+public class test1 {
+    public static void main(String[] args){
+        int first = 400, second = 700, result;
+        result = first + second;
+        if(result>1000){
+            second = second + 100;
+        }
+        System.out.println("The result is: "+ result);
+    }
+}
